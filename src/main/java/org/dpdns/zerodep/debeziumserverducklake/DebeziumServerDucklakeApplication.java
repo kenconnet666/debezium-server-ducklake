028d44c7e4f0e752d234895aa0a5414872282bcd
lionheart <2451809708@qq.com>
2026-07-10 15:05:57 +0800

    Initial commit

---
+++ src/main/java/org/dpdns/zerodep/debeziumserverducklake/DebeziumServerDucklakeApplication.java
@@ -0,0 +1,13 @@
+package org.dpdns.zerodep.debeziumserverducklake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumServerDucklakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumServerDucklakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dpdns/zerodep/debeziumserverducklake/DebeziumServerDucklakeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dpdns.zerodep.debeziumserverducklake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumServerDucklakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
